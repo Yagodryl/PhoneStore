0bfd7cbecfae6dd697186bd31de4d6dcb682621c
Horbatiuk <Horbatiuk@A34COMP1>
2018-10-31 14:55:25 +0200

    Start Create UI

---
+++ PhoneStore/UI/UIOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+   public class UIOrder
+    {
+        public int Id { get; set; }
+        public UIPhone Phone { get; set; }
+        public UIUser User { get; set; }
+
+    }
+}
