305e290bb69917807eeac5cc31c211694d6871a4
user <user@A34COMP4>
2018-10-31 14:56:00 +0200

    Create Contracts

---
+++ PhoneStore/BLL/BLLOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BLLOrder
+    {
+        public int Id { get; set; }
+        public BLLPhone Phone { get; set; }
+        public BLLUser User { get; set; }
+
+    }
+}
---
+++ PhoneStore/BLL/Contracts/ICRUD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Contracts
+{
+    [ServiceContract]
+    public interface ICRUD<T> where T:class 
+    {
+        [OperationContract]
+        void Create(T x);
+        [OperationContract]
+        void Delete(T x);
+        [OperationContract]
+        void Update(T x);
+        [OperationContract]
+        IEnumerable<T> Get();
+    }
+}
