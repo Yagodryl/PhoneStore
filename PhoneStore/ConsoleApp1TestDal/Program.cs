e67f399c7c3787cb66e476fd4a5e368dbca35fcb
antias1tn <antias1tn@outlook.com>
2018-11-01 14:41:10 +0200

    Add Repository

---
+++ PhoneStore/ConsoleApp1TestDal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace ConsoleApp1TestDal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DALUser user = new DALUser() { Name = "Petro", Adress = "Petro@ukr.net" };
+            Repository<DALUser> repository = new Repository<DALUser>(new Context());
+            List<DALUser> users = new List<DALUser>();
+            repository.Delete(user);
+            users = repository.GetAll().ToList();
+
+            foreach(var a in users)
+            {
+                Console.WriteLine(a.Name);
+            }
+        }
+    }
+}
---
+++ PhoneStore/DAL/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _set;
+
+        public Repository(DbContext db)
+        {
+            _context = db;
+            _set = db.Set<T>();
+        }
+
+        public void Create(T entity)
+        {
+            _set.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _set.Attach(entity);
+            _set.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _set.AsNoTracking<T>().AsEnumerable();
+        }
+
+        public T GetById(int id)
+        {
+            return _set.Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry<T>(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
